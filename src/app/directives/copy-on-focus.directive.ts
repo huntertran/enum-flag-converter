925a9ea35e39b75d61f4cf79f81de02e4a7913af
Tuan Tran <4468525+huntertran@users.noreply.github.com>
2023-09-30 17:47:34 -0400

    upgrading angular

--- src/app/directives/copy-on-focus.directive.ts
+++ src/app/directives/copy-on-focus.directive.ts
@@ -1,5 +1,5 @@
 import {Directive, ElementRef, HostListener} from '@angular/core';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import {MatLegacySnackBar as MatSnackBar} from '@angular/material/legacy-snack-bar';
 
 @Directive({
     selector: '[copyOnClick]'
